8a35818733800978bc591b6b01a48053707d51df
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2016-03-11 00:09:31 +0100

    was vergessen..

---
+++ mko.NaLisp/Factories/Ctrl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mko.NaLisp.Factories
+{
+    public partial class Ctrl
+    {
+        public static Ctrl _ = new Ctrl();
+
+        public Control.IfThen IfThen(Core.INaLisp Condition, Core.INaLisp EvalIfTrue, Core.INaLisp EvalIfFalse)
+        {
+            return new Control.IfThen(new Core.INaLisp[] { Condition, EvalIfTrue, EvalIfFalse });
+        }
+
+        public Control.Pipe Pipe(params Core.INaLisp[] Elements)
+        {
+            return new Control.Pipe(Elements);
+        }
+
+
+    }
+}
