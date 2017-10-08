2832feb34ec29e11ef0c69222ca62f06678a5820
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2017-10-08 20:55:42 +0200

    Starts implementing a classificator for caching

---
+++ MkPrgNet.Tools.Async/ClassifyByComponents.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MkPrgNet.Tools.Async
+{
+    public class ClassifyByComponents : IClassifier
+    {
+        Func<int, int, bool>[] eqFuncs;
+        Queue<int[]> history = new Queue<int[]>();
+
+        List<Tuple<int, int[]>> Categories;
+        List<int> matchesPerRow;
+
+        readonly int maxHistory;
+
+        public ClassifyByComponents(int maxHistory,  params Func<int, int, bool>[] eqFuncs)
+        {
+            this.maxHistory = maxHistory;
+            this.eqFuncs = new Func<int, int, bool>[eqFuncs.Count()];
+            Array.Copy(eqFuncs, this.eqFuncs, this.eqFuncs.Length);
+        }
+
+        /// <summary>
+        /// Classifies the vector. Returns a category to which the vector is assigned.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        public int CategoryOf(params int[] vector)
+        {
+            mko.TraceHlp.ThrowArgExIfNot(vector.Length > eqFuncs.Length, "vector.length > eqFuncs.lenght!");
+
+            // test equality for each category
+            for(int i = 0, count = Categories.Count; i < count; i++)
+            {
+                // count equal matches per category
+                matchesPerRow[i] = 0;
+                for(int j = 0, countCol = vector.Length; j < countCol; j++)
+                {
+                    matchesPerRow[i] += eqFuncs[j](Categories[i].Item2[j], vector[j]) ? 1 : 0;
+                }
+            }
+
+            // find category with max matches
+            int ix_max = -1, max = int.MinValue;
+            for(int i = 0, count = matchesPerRow.Count; i < count; i++)
+            {
+                if(matchesPerRow[i] >= max)
+                {
+                    max = matchesPerRow[i];
+                    ix_max = i;
+                }
+            }
+
+            return Categories[ix_max].Item1;           
+        }
+
+        public void Classify(params int[] vector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MkPrgNet.Tools.Async/IClassifier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MkPrgNet.Tools.Async
+{
+    /// <summary>
+    /// Classfactory for Classifiers
+    /// </summary>
+    public interface IClassifierBuilder
+    {       
+        IClassifier Create();
+    }
+
+    /// <summary>
+    /// Classfactory for special classifiers.
+    /// 
+    /// </summary>
+    public interface IClassifyByComponentsBuilder : IClassifierBuilder
+    {
+        /// <summary>
+        /// Defines a set of equality- operators for each component of a vector
+        /// </summary>
+        /// <param name="eqFuncs">Equal operator. Returns true if param 1 and param 2 are equal.</param>
+        void DefineEqualFunctions(params Func<int, int, bool>[] eqFuncs);
+    }
+
+    /// <summary>
+    /// Abstract structure of a classifier
+    /// </summary>
+    public interface IClassifier
+    {
+        /// <summary>
+        /// Maps vector to a category, based on implemented categorization algorithm and
+        /// defined equality operators for each component.
+        /// The input parameter, called vector, is an abstraction of object with n 
+        /// properties. In a previous step, the properties of the object were mapped 
+        /// to a list of integer values.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns>category to which the vector is assigned</returns>
+        int CategoryOf(params int[] vector);
+
+        /// <summary>
+        /// Trains the classification algorithm.
+        /// </summary>
+        /// <param name="vector"></param>
+        void Classify(params int[] vector);
+
+    }
+}
