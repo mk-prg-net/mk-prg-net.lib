099e9e1252eaff0ef9367934eab5fd0bbdd38d6a
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2019-01-25 00:22:50 +0100

    IdSet und IdGen def.

---
+++ MkPrgNet.Woc/IIdSet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MkPrgNet.Woc
+{
+    /// <summary>
+    /// mko, 24.1.2019
+    /// WocId's als hirarchisch verschachtelte Intervalle/Schlüsselmengen
+    /// </summary>
+    public interface IIdSet
+    {
+        /// <summary>
+        /// Informelle Kurzbeschreibung der Inhalte, die hinter dem IdSet stehen
+        /// </summary>
+        string Title { get; }
+
+        long Begin { get; }
+
+        long End { get; }
+
+        bool Contains(IIdSet set);        
+
+        // Setter für ein IdSet
+        void SetTo(long Begin, long End, string Title);
+
+    }
+}
---
+++ MkPrgNet.Woc/IdSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MkPrgNet.Woc
+{
+
+    /// <summary>
+    /// mko, 24.1.2019
+    /// Ist ein Intervall, welches ein Themengebit identifiziert analog einem Schlüssel.
+    /// Alle subthemen werden durch Intervalle präsentiert, die in diesem enthalten sind.
+    /// </summary>
+    public struct IdSet : IIdSet
+    {
+        MkPrgNet.Math.Sets.Interval<long> Interval;
+
+        public IdSet(long Begin, long End, string Title)
+        {
+            _Title = Title;
+            Interval.Begin = Begin;
+            Interval.End = End;
+        }
+
+        public string Title => _Title;
+        string _Title;
+
+        public long Begin => Interval.Begin;
+
+        public long End => Interval.End;
+
+        public bool Contains(IIdSet set)
+        {
+            return Interval.Contains(set.Begin) && Interval.Contains(set.End);
+        }
+
+        public void SetTo(long Begin, long End, string Title)
+        {
+            Interval = new Math.Sets.Interval<long>(Begin, End);
+            _Title = Title;
+        }
+    }
+}
